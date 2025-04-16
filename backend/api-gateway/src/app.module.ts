15aa2d91f7809789dacf6a949808bcccb545eeaa
baoispro <nguyenlegiabao810@gmail.com>
2025-04-16 22:48:36 +0700

    [feature/healthmart-44] add auth module to app module of api gateway

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { OrderModule } from './modules/order/order.module';
 import { ProductsModule } from './modules/products/products.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, OrderModule, ProductsModule],
+  imports: [UserModule, OrderModule, ProductsModule, AuthModule],
 })
 export class AppModule {}
