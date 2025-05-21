3f4c237e0f39f5e57947870c898b953c1d8098e6
baoispro <nguyenlegiabao810@gmail.com>
2025-05-21 23:04:20 +0700

    feat: add rate limiter client

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -6,11 +6,21 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PolicyModule } from './modules/policy/policies.module';
 import { ReviewModule } from './modules/review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // RẤT QUAN TRỌNG
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
     }),
     UserModule,
     OrderModule,
@@ -18,6 +28,12 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     PolicyModule,
     ReviewModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
